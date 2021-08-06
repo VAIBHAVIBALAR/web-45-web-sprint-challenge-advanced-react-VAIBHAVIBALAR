113a67cfef4f30ee8587a9fe9b933e97bfddd44f
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-05 20:21:50 -0700

    finalcodegradetestpasscommit

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import React, {useState} from "react";
+
+//Created this useForm custom hook and used the stateful logic required for checkout
+const useForm = (init) =>{
+    const [values, setValues] = useState(init);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  return [values, handleChanges]
+}
+export default useForm;
